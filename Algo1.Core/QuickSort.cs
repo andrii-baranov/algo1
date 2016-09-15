327ecc827c3c0c51579b48595f854f1ec8cab4ee
Andrii Baranov <andrii.baranov@gmail.com>
2016-09-15 13:04:56 +0300

    Basic quicksort implementation with first element as pivot selection

---
+++ Algo1.Core/QuickSort.cs
@@ -0,0 +1,54 @@
+﻿namespace Algo1.Core
+{
+    public class QuickSort : ISorter
+    {
+        public int[] Sort(int[] input)
+        {
+            if (input == null || input.Length == 0)
+            {
+                return input;
+            }
+            else
+            {
+                Sort(input, 0, input.Length - 1);
+
+                return input;
+            }
+        }
+
+        private void Sort(int[] array, int startIndex, int endIndex)
+        {
+            if (startIndex < endIndex)
+            {
+                var pivot = Partition(array, startIndex, endIndex);
+                Sort(array, startIndex, pivot);
+                Sort(array, pivot + 1, endIndex);
+            }
+        }
+
+        private int Partition(int[] array, int low, int high)
+        {
+            int pivot = array[low];
+            int j = low;
+
+            for (int i = j + 1; i <= high; i++)
+            {
+                if (array[i] < pivot)
+                {
+                    j++;
+                    Swap(array, i, j);
+                }
+            }
+            Swap(array, low, j);
+
+            return j;
+        }
+
+        private void Swap(int[] array, int firstIndex, int secondIndex)
+        {
+            var temp = array[firstIndex];
+            array[firstIndex] = array[secondIndex];
+            array[secondIndex] = temp;
+        }
+    }
+}
---
+++ Algo1.UnitTests/QuickSortTests.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using Algo1.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Algo1.UnitTests
+{
+    [TestClass]
+    public class QuickSortTests
+    {
+        [TestMethod]
+        public void SortNullArray()
+        {
+            ISorter sort = new QuickSort();
+
+            var result = sort.Sort(null);
+
+            Assert.IsNull(result);
+        }
+
+        [TestMethod]
+        public void SortEmptyArray()
+        {
+            ISorter sort = new QuickSort();
+
+            var result = sort.Sort(new int[0]);
+
+            Assert.IsNotNull(result);
+            Assert.IsTrue(result.Length == 0);
+        }
+
+
+        [TestMethod]
+        public void SortArrayOneElement()
+        {
+            ISorter sort = new QuickSort();
+
+            var input = new int[1];
+            input[0] = 10;
+            var result = sort.Sort(input);
+
+            Assert.IsNotNull(result);
+            Assert.IsTrue(result.Length == 1);
+            Assert.IsTrue(input[0] == result[0]);
+        }
+
+        [TestMethod]
+        public void SortArrayTwoElementsSorted()
+        {
+            ISorter sort = new QuickSort();
+
+            var input = new int[2] { 2, 4 };
+
+            var result = sort.Sort(input);
+
+            Assert.IsNotNull(result);
+            Assert.IsTrue(result.Length == input.Length);
+            Assert.IsTrue(input[0] == result[0]);
+            Assert.IsTrue(input[1] == result[1]);
+        }
+
+        [TestMethod]
+        public void SortArray8ElementsSorted()
+        {
+            ISorter sort = new QuickSort();
+
+            var input = new int[] { 6, 5, 3, 1, 7, 8, 2, 4 };
+
+            var result = sort.Sort(input);
+
+            Assert.IsNotNull(result);
+            Assert.IsTrue(result.Length == input.Length);
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                Assert.IsTrue(result[i] == i + 1);
+            }
+        }
+
+        [TestMethod]
+        public void SortArrayTwoElementsInversedSorted()
+        {
+            ISorter sort = new QuickSort();
+
+            var input = new int[2] { 4, 2 };
+
+            var result = sort.Sort(input);
+
+            Assert.IsNotNull(result);
+            Assert.IsTrue(result.Length == input.Length);
+            Assert.IsTrue(result[0] == 2);
+            Assert.IsTrue(result[1] == 4);
+        }
+    }
+}
