689a456d9236b2abd6bc5484bd0a069d0e238d91
Andrii Baranov <andsoulnd@gmail.com>
2016-09-19 23:37:59 +0300

    Added pivot selection to quicksort

--- Algo1.Console/Program.cs
+++ Algo1.Console/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Algo1.Core;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +12,29 @@ namespace Algo1.Console
     {
         static void Main(string[] args)
         {
+
+            var inputLines = File.ReadAllLines(@"C:\temp\_32387ba40b36359a38625cbb397eee65_QuickSort.txt");
+
+            int[] input = new int[inputLines.Length];
+
+            for (int i = 0; i < inputLines.Length; i++)
+            {
+                input[i] = int.Parse(inputLines[i]);
+            }
+
+            var quickSort = new QuickSort(PivotSelectionType.Median);
+
+            var result = quickSort.Sort(input);
+
+            var compCount = quickSort.ComparisonsCount;
+            for (int i =0; i < result.Length; i++)
+            {
+                if (result[i] != i + 1)
+                {
+                    System.Console.WriteLine("not match");
+                }
+            }
+
             System.Console.ReadKey();
         }
     }
--- Algo1.Core/QuickSort.cs
+++ Algo1.Core/QuickSort.cs
@@ -1,9 +1,28 @@
-﻿namespace Algo1.Core
+﻿using System;
+
+namespace Algo1.Core
 {
     public class QuickSort : ISorter
     {
+        private PivotSelectionType _pivotType;
+        private Func<int[], int, int, int> _partitionFunc;
+
+        public QuickSort() : this(PivotSelectionType.Median)
+        {
+        }
+        public QuickSort(PivotSelectionType pivotSelectionType)
+        {
+            _pivotType = pivotSelectionType;
+            _partitionFunc = GetPartitionFunc(_pivotType);
+        }
+        /// <summary>
+        /// Holds number of comparisons done during the sorting
+        /// </summary>
+        public long ComparisonsCount;
         public int[] Sort(int[] input)
         {
+            ComparisonsCount = 0;
+
             if (input == null || input.Length == 0)
             {
                 return input;
@@ -20,13 +39,28 @@
         {
             if (startIndex < endIndex)
             {
-                var pivot = Partition(array, startIndex, endIndex);
-                Sort(array, startIndex, pivot);
+                var pivot = _partitionFunc(array, startIndex, endIndex);
+                ComparisonsCount += (pivot - 1 - startIndex);
+                Sort(array, startIndex, pivot-1);
+                ComparisonsCount += (endIndex - pivot + 1);
                 Sort(array, pivot + 1, endIndex);
             }
         }
 
-        private int Partition(int[] array, int low, int high)
+        private Func<int[], int, int, int> GetPartitionFunc(PivotSelectionType pivotSelector)
+        {
+            switch (pivotSelector)
+            {
+                case PivotSelectionType.First:
+                    return this.PartitionViaLowPivot;
+                case PivotSelectionType.Last:
+                    return this.PartitionViaHighPivot;
+                default:
+                case PivotSelectionType.Median:
+                    return PartitionViaMedianPivot;
+            }
+        }
+        private int PartitionViaLowPivot(int[] array, int low, int high)
         {
             int pivot = array[low];
             int j = low;
@@ -44,6 +78,94 @@
             return j;
         }
 
+        // end
+        private int PartitionViaHighPivot(int[] array, int low, int high)
+        {
+            Swap(array, low, high);
+
+            int pivot = array[low];
+            int j = low;
+
+            for (int i = j + 1; i <= high; i++)
+            {
+                if (array[i] < pivot)
+                {
+                    j++;
+                    Swap(array, i, j);
+                }
+            }
+            Swap(array, low, j);
+
+            return j;
+        }
+        // partition via median pivot
+        private int PartitionViaMedianPivot(int[] array, int low, int high)
+        {
+            int pivotIndex = FindMedianPivotIndex(array, low, high);
+
+            Swap(array, low, pivotIndex);
+
+            int pivot = array[low];
+
+            int j = low;
+
+            for (int i = j + 1; i <= high; i++)
+            {
+                if (array[i] < pivot)
+                {
+                    j++;
+                    Swap(array, i, j);
+                }
+            }
+            Swap(array, low, j);
+
+            return j;
+        }
+        private int FindMedianPivotIndex(int[] array, int low, int high)
+        {
+            int length = high - low + 1;
+            int middlePos = 0;
+            if (length % 2 == 0)
+            {
+                middlePos = low + length / 2 - 1;
+            }
+            else
+            {
+                middlePos = low + length / 2;
+            }
+
+            int firstPos = low;
+            int lastPos = high;
+
+            int first = array[firstPos];
+            int middle = array[middlePos];
+            int last = array[lastPos];
+
+            if (first <= middle)
+            {
+                if (middle <= last)
+                {
+                    return middlePos;
+                }
+                else if (first <= last)
+                {
+                    return lastPos;
+                }
+                return firstPos;
+            }
+
+            if (first <= last)
+            {
+                return firstPos;
+            }
+            else if (middle <= last)
+            {
+                return lastPos;
+            }
+
+            return middlePos;
+        }
+        
         private void Swap(int[] array, int firstIndex, int secondIndex)
         {
             var temp = array[firstIndex];
