7dc776b934613e76da318b3094bdda554d9ff777
Andrii Baranov <andsoulnd@gmail.com>
2016-10-10 23:50:47 +0300

    Dijkstra graph search

---
+++ Algo1.Core/DijkstraPathSearch.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Algo1.Core
+{
+    public class DijkstraPathSearch
+    {
+        List<AdjacencyItem> _graph;
+        public DijkstraPathSearch()
+        {
+            _graph = new List<AdjacencyItem>();
+        }
+
+        public void AddNode(AdjacencyItem item)
+        {
+            _graph.Add(item);
+        }
+
+        public void CalculateShortestPath(int startNodeNo)
+        {
+            var startNode = _graph.FirstOrDefault(n => n.NodeId == startNodeNo);
+            if (startNode == null)
+            {
+                throw new ArgumentException("startNode is not found in the graph");
+            }
+
+            startNode.CurrentCost = 0;
+
+            List<AdjacencyItem> nodesToVisit = new List<AdjacencyItem>();
+
+            nodesToVisit.Add(startNode);
+
+            while (nodesToVisit.Count > 0)
+            {
+                //todo ugly fix, change with heap structure to enable ordered storage
+                nodesToVisit = nodesToVisit.OrderBy(i => i.CurrentCost).ToList();
+                var currentNode = nodesToVisit[0];
+                nodesToVisit.RemoveAt(0);
+           
+                if (currentNode.IsVisited)
+                {
+                    continue;
+                }
+                // nodes numbering starts from 1, need to adjust
+                var linkedNodes = _graph[currentNode.NodeId-1].LinkedNodes;
+
+                foreach(var node in linkedNodes)
+                {
+                    var discoveredNode = _graph[(node.Key) -1];
+
+                    if (!discoveredNode.IsVisited)
+                    {
+                        if (currentNode.CurrentCost + node.Value < discoveredNode.CurrentCost)
+                        {
+                            discoveredNode.CurrentCost = currentNode.CurrentCost + node.Value;
+                        }
+
+                        nodesToVisit.Add(discoveredNode);
+                    }
+                }
+
+                currentNode.IsVisited = true;
+            }
+            
+       
+        }
+
+        public List<AdjacencyItem> GraphInfo
+        {
+            get
+            {
+                return _graph;
+            }
+        }
+    }
+
+    public class AdjacencyItem
+    {
+        public AdjacencyItem()
+        {
+            LinkedNodes = new Dictionary<int, int>();
+            CurrentCost = int.MaxValue;
+        }
+        public int NodeId { get; set; }
+
+        public bool IsVisited { get; set; }
+
+        public int CurrentCost { get; set; }
+
+        public Dictionary<int, int> LinkedNodes { get; set; }
+
+        public static AdjacencyItem ParseNodeInfo(string nodeString)
+        {
+            var items = nodeString.Split('\t', ' ');
+
+            AdjacencyItem graphItem = new AdjacencyItem();
+
+            graphItem.NodeId = Int32.Parse(items[0]);
+
+            var connectedNodesInfo = items.Skip(1); // skipping nodeId
+
+            foreach (var nodeIndo in connectedNodesInfo)
+            {
+                if (!string.IsNullOrWhiteSpace(nodeIndo))
+                {
+                    var splitInfo = nodeIndo.Split(',');
+                    var node = Int32.Parse(splitInfo[0]);
+                    var cost = Int32.Parse(splitInfo[1]);
+
+                    graphItem.LinkedNodes.Add(node, cost);
+                } else
+                {
+
+                }
+            }
+
+            return graphItem;
+        }
+    }
+}
---
+++ Algo1.UnitTests/DijkstraSearchTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Algo1.Core;
+using System.Linq;
+
+namespace Algo1.UnitTests
+{
+    /// <summary>
+    /// Summary description for DijkstraSearchTests
+    /// </summary>
+    [TestClass]
+    public class DijkstraSearchTests
+    {
+        [TestMethod]
+        public void InitializeGraph()
+        {
+            // test input
+            var line1 = "1 2,1 3,4";
+            var line2 = "2 1,1 3,2 4,6";
+            var line3 = "3 1,4 2,2 4,3";
+            var line4 = "4 2,6 3,3";
+
+            var pathFinder = new DijkstraPathSearch();
+
+            pathFinder.AddNode(AdjacencyItem.ParseNodeInfo(line1));
+            pathFinder.AddNode(AdjacencyItem.ParseNodeInfo(line2));
+            pathFinder.AddNode(AdjacencyItem.ParseNodeInfo(line3));
+            pathFinder.AddNode(AdjacencyItem.ParseNodeInfo(line4));
+
+            Assert.IsTrue(pathFinder.GraphInfo.Count == 4);
+        }
+
+        [TestMethod]
+        public void FindShortestPathTest()
+        {
+            // test input
+            var line1 = "1 2,1 3,4";
+            var line2 = "2 1,1 3,2 4,6";
+            var line3 = "3 1,4 2,2 4,3";
+            var line4 = "4 2,6 3,3";
+
+            var pathFinder = new DijkstraPathSearch();
+
+            pathFinder.AddNode(AdjacencyItem.ParseNodeInfo(line1));
+            pathFinder.AddNode(AdjacencyItem.ParseNodeInfo(line2));
+            pathFinder.AddNode(AdjacencyItem.ParseNodeInfo(line3));
+            pathFinder.AddNode(AdjacencyItem.ParseNodeInfo(line4));
+
+            pathFinder.CalculateShortestPath(1);
+            Assert.IsTrue(pathFinder.GraphInfo[0].CurrentCost == 0);
+            Assert.IsTrue(pathFinder.GraphInfo[1].CurrentCost == 1);
+            Assert.IsTrue(pathFinder.GraphInfo[2].CurrentCost == 3);
+            Assert.IsTrue(pathFinder.GraphInfo[3].CurrentCost == 6);
+        }
+    }
+}
