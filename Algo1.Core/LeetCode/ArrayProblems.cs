1135382e64c45767205a0cab5ecb00af831d6614
Andrii Baranov <andsoulnd@gmail.com>
2017-02-03 02:07:45 +0200

    LeetCode 57 problem draft and tests

---
+++ Algo1.Core/LeetCode/ArrayProblems.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algo1.Core.LeetCode
+{
+    public class ArrayProblems
+    {
+        // LeetCode 57. Insert Interval
+        public IList<Interval> Insert(IList<Interval> intervals, Interval newInterval)
+        {
+            int curIndex = 0;
+            int endIndex = 0;
+            int newEnd = newInterval.end;
+            int newStart = newInterval.start;
+            List<Interval> result = new List<Interval>();
+
+            if (intervals.Count == 0 ||intervals[curIndex].start > newInterval.end)
+            {
+               // edge case, no overlapping, add to start
+                result.Add(newInterval);
+                result.AddRange(intervals);
+
+                return result;
+            }
+
+            if (intervals.Last().end < newInterval.start)
+            {
+                // edge case, no intersection - add to end
+                result.AddRange(intervals);
+                result.Add(newInterval);
+                
+                return result;
+            }
+
+
+            return result;
+
+            //// find interscetion start TODO fix
+            //while (curIndex  < intervals.Count
+            //      && (intervals[curIndex].start > newEnd || intervals[curIndex].end < newStart)
+            //    )
+            //{
+            //    curIndex++;
+            //}
+
+            //if (intervals[curIndex].end < newStart)
+            //{
+            //    // instert after TODO
+
+            //    result.AddRange(intervals.Take(curIndex + 1));
+            //    result.Add(newInterval);
+            //    result.AddRange(intervals.Skip(curIndex+1));
+
+            //    return result;
+            //}
+
+
+            //// merge start point
+            //newStart = Math.Min(intervals[curIndex].start, newStart);
+            //newEnd = Math.Max(newEnd, intervals[curIndex].end);
+            //endIndex = curIndex;
+
+            //while (endIndex + 1 < intervals.Count && newEnd >= intervals[endIndex+1].start)
+            //{
+            //    newEnd = Math.Max(newEnd, intervals[endIndex + 1].end);
+            //    endIndex++;
+            //}
+
+            //// curIndex - start of merged items,
+            //// endIndex - end of merged items
+            //newInterval.start = newStart;
+            //newInterval.end = newEnd;
+
+            //result.AddRange(intervals.Take(curIndex));
+            //result.Add(newInterval);
+            //result.AddRange(intervals.Skip(endIndex + 1));
+
+            //return result;
+        }
+
+        public class Interval
+        {
+            public int start;
+            public int end;
+            public Interval() { start = 0; end = 0; }
+            public Interval(int s, int e) { start = s; end = e; }
+        }
+    }
+}
---
+++ Algo1.UnitTests/ArrayProblemTests.cs
@@ -0,0 +1,103 @@
+﻿using Algo1.Core.LeetCode;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Algo1.Core.LeetCode.ArrayProblems;
+
+namespace Algo1.UnitTests
+{
+    [TestClass]
+    public class ArrayProblemTests
+    {
+        [TestMethod]
+        public void InsertIntervalTests()
+        {
+            var arrProblem = new ArrayProblems();
+            List<Interval> intervals = new List<Interval>();
+
+            intervals.Add(new Interval(1, 2));
+            intervals.Add(new Interval(3, 5));
+            intervals.Add(new Interval(6, 7));
+            intervals.Add(new Interval(8, 10));
+            intervals.Add(new Interval(12, 16));
+
+            var intervalToMerge = new Interval(5, 7);
+
+            var result = arrProblem.Insert(intervals, intervalToMerge);
+
+            Assert.AreEqual(result.Count, 4);
+        }
+
+        [TestMethod]
+        public void InsertIntervalTestToStart()
+        {
+            var arrProblem = new ArrayProblems();
+            List<Interval> intervals = new List<Interval>();
+
+            intervals.Add(new Interval(2, 2));
+            intervals.Add(new Interval(3, 5));
+            intervals.Add(new Interval(6, 7));
+            intervals.Add(new Interval(8, 10));
+            intervals.Add(new Interval(12, 16));
+
+            var intervalToMerge = new Interval(0, 1);
+
+            var result = arrProblem.Insert(intervals, intervalToMerge);
+
+            Assert.AreEqual(result.Count, 6);
+        }
+
+        [TestMethod]
+        public void InsertIntervalTestToMiddle()
+        {
+            var arrProblem = new ArrayProblems();
+            List<Interval> intervals = new List<Interval>();
+
+            intervals.Add(new Interval(2, 2));
+            intervals.Add(new Interval(3, 5));
+            intervals.Add(new Interval(6, 7));
+            intervals.Add(new Interval(8, 9));
+            intervals.Add(new Interval(12, 16));
+
+            var intervalToMerge = new Interval(10, 11);
+
+            var result = arrProblem.Insert(intervals, intervalToMerge);
+
+            Assert.AreEqual(result.Count, 6);
+        }
+
+        [TestMethod]
+        public void InsertIntervalTestToBack()
+        {
+            var arrProblem = new ArrayProblems();
+            List<Interval> intervals = new List<Interval>();
+
+            intervals.Add(new Interval(2, 2));
+            intervals.Add(new Interval(3, 5));
+
+            var intervalToMerge = new Interval(6, 8);
+
+            var result = arrProblem.Insert(intervals, intervalToMerge);
+
+            Assert.AreEqual(result.Count, 3);
+        }
+
+        [TestMethod]
+        public void InsertIntervalTestToEmpty()
+        {
+            var arrProblem = new ArrayProblems();
+            List<Interval> intervals = new List<Interval>();
+
+
+            var intervalToMerge = new Interval(6, 8);
+
+            var result = arrProblem.Insert(intervals, intervalToMerge);
+
+            Assert.AreEqual(result.Count, 1);
+        }
+
+    }
+}
